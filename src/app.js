d8b25211616e3b0817516fb99a3ee8a7f5c69277
kriviears <kriviears@gmail.com>
2020-02-15 23:36:54 -0500

    prep for heroku deploy

--- src/app.js
+++ src/app.js
@@ -1,46 +1,42 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const characterRouter = require('./characters/character-router');
-const noteRouter = require('./notes/notes-router');
-const movesRouter = require('./moves/moves-router');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const characterRouter = require("./characters/character-router");
+const noteRouter = require("./notes/notes-router");
+const movesRouter = require("./moves/moves-router");
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function errorHandler(error, req, res, next){
+app.use(function errorHandler(error, req, res, next) {
   let response;
-  if(NODE_ENV === 'production'){
-    response = { error: { message: 'server error'} };
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
     console.error(error);
-    response = { message: error.message, error};
+    response = { message: error.message, error };
   }
   res.status(500).json(response);
 });
 
-app.get('/', (req, res)=>{
-  res.send('Embrace frames');
+app.get("/", (req, res) => {
+  res.send("Samurai Shodown api");
 });
 
-app.use('/api/moves', movesRouter);
-app.use('/api/characters', characterRouter);
-app.use('/api/notes', noteRouter);
+app.use("/api/moves", movesRouter);
+app.use("/api/characters", characterRouter);
+app.use("/api/notes", noteRouter);
 //app.use('/api/info', infoRouter);
 //app.use('/api/users', usersRouter);
-
 
 module.exports = app;
