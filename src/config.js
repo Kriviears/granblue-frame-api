247e5f62399fbe5c10cb16c8b1b997baa3698f79
kriviears <kriviears@gmail.com>
2020-02-20 14:25:45 -0500

    working on heroku

--- src/config.js
+++ src/config.js
@@ -7,5 +7,8 @@ module.exports = {
     process.env.DATABASE_URL ||
     "postgresql://PersonalProj:ATLFGC1234@localhost/samsho-reactable",
   JWT_SECRET: process.env.JWT_SECRET || "secret-secure",
-  JWT_EXPIRY: process.env.JWT_EXPIRY || "20s"
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "20s",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://PersonalProj:ATLFGC1234@localhost/samsho-reactable"
 };
